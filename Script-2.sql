41ce8f30f11166cea2b1733104d33009094f5adc
v <daderiovictorfelipe@gmail.com>
2026-02-01 20:57:01 -0300

    slq inicial

---
+++ Script-2.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	username text NOT NULL,
+	email text UNIQUE NOT NULL,
+	password_hash text NOT NULL,
+	created_at timestamp DEFAULT now()
+);
+
+CREATE TABLE items (
+	id serial PRIMARY KEY,
+	nome text NOT NULL,
+	descricao text NOT NULL,
+	preco numeric(10, 2) NOT NULL,
+	image_url text,
+	tipo text DEFAULT 'servico' CHECK (tipo IN ('servico', 'produto'))	
+);
+
+ALTER TABLE users
+ADD COLUMN updated_at timestamp DEFAULT now();
+
+ALTER TABLE items
+ADD COLUMN updated_at timestamp DEFAULT now(),
+ADD COLUMN updated_by integer REFERENCES users(id); -- Quem alterou
+
+CREATE TABLE items_logs (
+	id serial PRIMARY KEY,
+	item_id integer REFERENCES items(id),
+	user_id integer REFERENCES users(id),
+	acao text NOT NULL, -- 'create', 'update', 'delete'
+	data_alteracao timestamp NOT NULL,
+	created_at timestamp DEFAULT now()
+);
+
+ALTER TABLE items
+ADD COLUMN estoque_atual integer DEFAULT 0,
+ADD COLUMN estoque_minimo integer DEFAULT 0;
+
+ALTER TABLE items_logs
+ADD COLUMN estoque_anterior integer DEFAULT 0,
+ADD COLUMN estoque_definido integer DEFAULT 0;
+
+SELECT * FROM items;
