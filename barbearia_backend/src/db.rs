046ae4eaa670af37928a8ca127cb9ca329d755f0
v <daderiovictorfelipe@gmail.com>
2026-02-02 23:53:52 -0300

    feita a conexão com banco de dados

---
+++ barbearia_backend/src/db.rs
@@ -0,0 +1,8 @@
+use sqlx::{PgPool, postgres::PgPoolOptions};
+use std::env;
+
+pub async fn create_pool() -> Result<PgPool, sqlx::Error> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be in .env environment");
+
+    PgPoolOptions::new().max_connections(5000).connect(&database_url).await
+}
--- barbearia_backend/src/main.rs
+++ barbearia_backend/src/main.rs
@@ -1,7 +1,12 @@
 mod models;
 mod handlers;
 mod response;
+mod db;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let pool = db::create_pool().await.expect("Falha to connect to database");
+    println!("Connected to PostgreSQL");
 }
