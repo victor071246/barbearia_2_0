a6c952815f904022b08eafadf7af1ca3bc75c768
v <daderiovictorfelipe@gmail.com>
2026-02-03 23:58:02 -0300

    esboço dos handlers

--- barbearia_backend/src/handlers/user_handler.rs
+++ barbearia_backend/src/handlers/user_handler.rs
@@ -1,5 +1,18 @@
 use axum::{Json, extract::Path, http::StatusCode};
-use crate::models::user::*;
-use crate::response::{ApiResponse};
+use crate::models::user::{CreateUserRequest, UpdateUserRequest, UserResponse};
+use crate::response::ApiResponse;
 
-pub async fn create_user(Json(payload): Json<CreateUserRequest>)
+//CREATE
+pub async fn create_user(Json(payload): Json<CreateUserRequest>) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
+    todo!()
+}
+
+//READ (um por ID)
+pub async fn get_user_by_id(Path(id): Path<i32>) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
+    todo!()
+}
+
+//UPDATE
+pub async fn update_user(Path(id): Path<i32>, Json(payload): Json<UpdateUserRequest>) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
+    todo!()
+}
