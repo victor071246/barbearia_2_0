c9fe20f80f0593dc82e303df080c6c890fd507d0
v <daderiovictorfelipe@gmail.com>
2026-02-07 23:50:49 -0300

    items routes added

--- barbearia_backend/src/routes/item_routes.rs
+++ barbearia_backend/src/routes/item_routes.rs
@@ -1,4 +1,4 @@
-use axum:: {
+use axum::{
     routing::{get, post, put, delete},
     Router,
 };
@@ -7,5 +7,15 @@ use crate::handlers::item_handler::{
     create_item,
     get_all_items,
     get_item_by_id,
-    update_item
+    update_item,
+    delete_item,
+};
+
+pub fn item_routes(pool: PgPool) -> Router {
+    Router::new()
+    .route("/items", post(create_item))
+    .route("/items", get(get_all_items))
+    .route("/items/:id", get(get_item_by_id))
+    .route("/items/:id", delete(delete_item))
+    .with_state(pool)
 }
